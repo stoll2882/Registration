14e12608320e2420e6ab3e3c6b4b8c637b0a2fce
Cole Thorpen <thorpencole@gmail.com>
2021-11-18 15:06:55 -0800

    working on models

--- registration/regserve/models.py
+++ registration/regserve/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core import validators
+from django.core.validators import MaxLengthValidator, MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Student(models.Model):
+    YEAR_IN_SCHOOL = [
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    ]
+    studentid = models.PositiveIntegerField(blank=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    schoolyear = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL)
