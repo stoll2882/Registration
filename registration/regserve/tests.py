9f007744fc7cc30de15a28dfde86ca837ecd23a3
Cole Thorpen <thorpencole@gmail.com>
2021-11-18 14:30:12 -0800

    sync changes

--- registration/regserve/tests.py
+++ registration/regserve/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.test_client = Client()
+
+    def test_response(self):
+        response = self.test_client.get('/regserve')
+        print(f'In SimpleTest, response is {response}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello world from Django backend!')
--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+application = 'regserve'
+
+
 urlpatterns = [
     path('', views.index, name="index"),
 ]
