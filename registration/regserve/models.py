799ba3b63e80096cdec249ab10628884311933fd
Cole Thorpen <thorpencole@gmail.com>
2021-11-30 15:08:20 -0800

    done with class 11/30

--- registration/regserve/models.py
+++ registration/regserve/models.py
@@ -2,8 +2,27 @@ from django.db import models
 from django.core import validators
 from django.core.validators import MaxLengthValidator, MinValueValidator, MaxValueValidator
 
+class Person(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    idnumber = models.PositiveBigIntegerField()
+    email = models.EmailField(blank=True)
+    datecreated = models.DateTimeField(blank=True, auto_now_add=True)
+    datemodified = models.DateTimeField(blank=True, auto_now=True)
+    
+    @property
+    def full_name(self):
+        return f'{self.firstname} {self.lastname}'
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'PersonID: {self.id}:, Name:{self.full_name}, Student ID: {self.idnumber}, Email: {self.email}, Date Created: {self.datecreated}, Date Modified: {self.datemodified} '
+
+
 # Create your models here.
-class Student(models.Model):
+class Student(Person):
     YEAR_IN_SCHOOL = [
         ('FR', 'Freshman'),
         ('SO', 'Sophomore'),
@@ -11,7 +30,22 @@ class Student(models.Model):
         ('SR', 'Senior'),
         ('GR', 'Graduate'),
     ]
-    studentid = models.PositiveIntegerField(blank=True)
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
+
+    MAJORS = [
+        ('CS', 'Computer Science'),
+        ('ENG', 'Engineering'),
+        ('BUS', 'Business'),
+        ('SC', 'Science'),
+        ('LAW', 'Law'),
+        ('NUR', 'Nursing'),
+        ('UND', 'Undecided'),
+    ]
+
     schoolyear = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL)
+    major = models.CharField(max_length=3, choices=MAJORS)
+    gpa = models.FloatField(max_length=4, blank=True)
+
+    def __str__(self):
+        return f'StudentID: {self.id}: {super(Student, self).__str__()} - Year in School: {self.schoolyear}, Major: {self.major}, GPA: {self.gpa} '
+
+    
--- registration/regserve/tests.py
+++ registration/regserve/tests.py
@@ -1,7 +1,40 @@
 from django.http import response
 from django.test import TestCase, Client
+from .models import *
+
 
 # Create your tests here.
+class DataTest(TestCase):
+    def setUp(self):
+        student1 = Student.objects.create(
+            firstname="First",
+             lastname="Student",
+             idnumber=100,
+             email="first@student.edu",
+             schoolyear="FR",
+             major="CS",
+             gpa="4.0",
+             )
+
+        student2 = Student.objects.create(
+            firstname="Second",
+             lastname="Student",
+             idnumber=101,
+             email="second@student.edu",
+             schoolyear="SR",
+             major="ENG",
+             gpa="2.0",
+             )
+
+    def test_student(self):
+        student_list = Student.objects.all()
+        student = student_list[0]
+        print(f'Inside test student: student it {student}')
+        self.assertEqual(student.id, 1)
+        self.assertEqual(student.full_name, 'First Student')
+        self.assertEqual(student.idnumber, 100)
+
+
 class SimpleTest(TestCase):
     def setUp(self):
         self.test_client = Client()
