60a0ea97abf058927a9ea40c4ceb747594d13d35
Sam Toll <sam.x.toll@gmail.com>
2021-12-09 14:57:48 -0800

    basis of project

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 application = 'regserve'
 
-
 urlpatterns = [
     path('', views.index, name="index"),
     path('/data/students/', views.StudentListView.as_view(), name='api_students'),
+    path('/students/', views.StudentListForm.as_view(), name='students'),
 ]
-
--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .serializers import *
 from rest_framework import generics
+from django.views.generic import ListView
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,6 @@ def index(request):
 class StudentListView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+class StudentListForm(ListView):
+    model = Student
